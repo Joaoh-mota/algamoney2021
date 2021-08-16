0f2585c5846cdf9eacb4bad0d3e6fe403a671599
Joaoh-mota <joaohenriquemota1@gmail.com>
2021-08-16 08:49:41 -0300

    Criacao da migração e da classe de Lancamento

---
+++ backend/algamoney-api/src/main/java/com/henrique/algamoney/api/model/TipoLancamento.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.henrique.algamoney.api.model;
+
+/**
+ *
+ * @author joaoh
+ */
+public enum TipoLancamento {
+    RECEITA,
+    DESPESA
+}
